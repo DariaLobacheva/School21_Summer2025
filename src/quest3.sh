cfa1187dbd5c82853485c8b9262ff5f9a98b8bef
Daria <58396165+DariaLobacheva@users.noreply.github.com>
2025-07-07 14:15:11 +0300

    Day1 Task 1
    First day | GIT | Bash

---
+++ src/quest3.sh
@@ -0,0 +1,8 @@
+cat door_1.log
+cd ..
+bash ai_door_management_module.sh
+bash ai_door_management_module.sh
+bash ai_door_control.sh
+bash ai_door_management_module.sh
+git add .
+git commit -m 'Second commit'
---
+++ src/quest6.sh
@@ -0,0 +1,5 @@
+ls
+cd ai_help
+ls
+bash keygen.sh
+bash unifier.sh
